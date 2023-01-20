c6e53976fda4e246b2c091d23a150847545291f2
ritwik1804 <103522580+ritwik1804@users.noreply.github.com>
2023-01-20 23:06:58 +0530

    Create Linear Search.c

---
+++ Linear Search.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+ 
+int linearSearch(int arr[], int size, int element){
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+ 
+int binarySearch(int arr[], int size, int element){
+    int low, mid, high;
+    low = 0;
+    high = size-1;
+  
+    while(low<=high){
+        mid = (low + high)/2;
+        if(arr[mid] == element){
+            return mid;
+        }
+        if(arr[mid]<element){
+            low = mid+1;
+        }
+        else{
+            high = mid -1;
+        }
+    } 
+    return -1;
+    
+}
+ 
+int main(){
+   
+    int arr[] = {1,3,5,56,64,73,123,225,444};
+    int size = sizeof(arr)/sizeof(int);
+    int element = 444;
+    int searchIndex = binarySearch(arr, size, element);
+    printf("The element %d was found at index %d \n", element, searchIndex);
+    return 0;
+}
