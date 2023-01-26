0256e95bc22dcf23c879577a1b6d9672eed04460
ritwik1804 <103522580+ritwik1804@users.noreply.github.com>
2023-01-26 21:52:11 +0530

    Create Linkedlist.c

---
+++ Linkedlist.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+ 
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+ 
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+ 
+   
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+ 
+    head->data = 7;
+    head->next = second;
+ 
+   
+    second->data = 11;
+    second->next = third;
+ 
+  
+    third->data = 41;
+    third->next = fourth;
+ 
+    
+    fourth->data = 66;
+    fourth->next = NULL;
+ 
+    linkedListTraversal(head);
+    return 0;
+}
