1f8414fe26984b0b9f448a392cb7b19f7b53abca
Ritwik Mohanty <103522580+ritwik1804@users.noreply.github.com>
2023-01-29 15:04:32 +0530

    Create DLLTraversal.c

---
+++ DLLTraversal.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *head){
+    struct Node *ptr = head;
+    do{
+        printf("Element is %d\n", ptr->data);
+        ptr = ptr->next;
+    }while(ptr!=head);
+}
+
+
+    p->next = ptr;
+    ptr->next = head;
+    head = ptr;
+    return head;
+
+}
+
+int main(){
+    
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 4;
+    head->next = second;
+
+
+    second->data = 3;
+    second->next = third;
+
+    
+    third->data = 6;
+    third->next = fourth;
+
+ 
+    fourth->data = 1;
+    fourth->next = head;
+
+    return 0;}
